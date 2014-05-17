9786a051f337998ad9d19c7083e2143df37131e3
monochromegane <dev.kuro.obi@gmail.com>
2014-05-18 01:24:01 +0900

    Added a head config.

--- pt.rb
+++ pt.rb
@@ -3,8 +3,9 @@ require 'formula'
 HOMEBREW_PT_VERSION='1.6.1'
 class Pt < Formula
   homepage 'https://github.com/monochromegane/the_platinum_searcher'
-  url 'https://github.com/monochromegane/the_platinum_searcher.git', :tag => "v#{HOMEBREW_PT_VERSION}" 
+  url 'https://github.com/monochromegane/the_platinum_searcher.git', :tag => "v#{HOMEBREW_PT_VERSION}"
   version HOMEBREW_PT_VERSION
+  head 'https://github.com/monochromegane/the_platinum_searcher.git', :branch => 'master'
 
   depends_on 'go' => :build
   depends_on 'hg' => :build
