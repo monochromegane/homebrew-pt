42bfb4cd85383309d6107a788e1c33531b4a30cd
monochromegane <dev.kuro.obi@gmail.com>
2014-07-23 00:26:02 +0900

    Install url is now changed to binary download url.

--- pt.rb
+++ pt.rb
@@ -3,22 +3,28 @@ require 'formula'
 HOMEBREW_PT_VERSION='1.7.0'
 class Pt < Formula
   homepage 'https://github.com/monochromegane/the_platinum_searcher'
-  url 'https://github.com/monochromegane/the_platinum_searcher.git', :tag => "v#{HOMEBREW_PT_VERSION}"
+  url "https://github.com/monochromegane/the_platinum_searcher/releases/download/v#{HOMEBREW_PT_VERSION}/pt_darwin_amd64.zip"
+  sha1 '7da6e90c5fbc92c69ac3ae439830e101fb5edd46'
+
   version HOMEBREW_PT_VERSION
   head 'https://github.com/monochromegane/the_platinum_searcher.git', :branch => 'master'
 
-  depends_on 'go' => :build
-  depends_on 'hg' => :build
+  if build.head?
+    depends_on 'go' => :build
+    depends_on 'hg' => :build
+  end
 
   def install
-    ENV['GOPATH'] = buildpath
-    system 'go', 'get', 'github.com/shiena/ansicolor'
-    system 'go', 'get', 'github.com/monochromegane/terminal'
-    system 'go', 'get', 'github.com/jessevdk/go-flags'
-    system 'go', 'get', 'code.google.com/p/go.text/transform'
-    mkdir_p buildpath/'src/github.com/monochromegane'
-    ln_s buildpath, buildpath/'src/github.com/monochromegane/the_platinum_searcher'
-    system 'go', 'build', '-o', 'pt', 'cmd/pt/main.go'
+    if build.head?
+      ENV['GOPATH'] = buildpath
+      system 'go', 'get', 'github.com/shiena/ansicolor'
+      system 'go', 'get', 'github.com/monochromegane/terminal'
+      system 'go', 'get', 'github.com/jessevdk/go-flags'
+      system 'go', 'get', 'code.google.com/p/go.text/transform'
+      mkdir_p buildpath/'src/github.com/monochromegane'
+      ln_s buildpath, buildpath/'src/github.com/monochromegane/the_platinum_searcher'
+      system 'go', 'build', '-o', 'pt', 'cmd/pt/main.go'
+    end
     bin.install 'pt'
   end
 end
