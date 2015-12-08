818269d1ff55d76445bc26532423835c0bf27b57
monochromegane <dev.kuro.obi@gmail.com>
2015-12-08 23:28:24 +0900

    Bumped version to 2.0.0.

--- pt.rb
+++ pt.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
-HOMEBREW_PT_VERSION='1.7.9'
+HOMEBREW_PT_VERSION='2.0.0'
 class Pt < Formula
   homepage 'https://github.com/monochromegane/the_platinum_searcher'
   url "https://github.com/monochromegane/the_platinum_searcher/releases/download/v#{HOMEBREW_PT_VERSION}/pt_darwin_amd64.zip"
-  sha1 'a59cc5fa697ed44b11a859fd2f47459f622ce318'
+  sha1 'cc15dd4c145aabb6a2d05699736871662f18d79f'
 
   version HOMEBREW_PT_VERSION
   head 'https://github.com/monochromegane/the_platinum_searcher.git', :branch => 'master'
@@ -21,6 +21,7 @@ class Pt < Formula
       system 'go', 'get', 'github.com/monochromegane/terminal'
       system 'go', 'get', 'github.com/monochromegane/go-home'
       system 'go', 'get', 'github.com/monochromegane/conflag'
+      system 'go', 'get', 'github.com/monochromegane/go-gitignore'
       system 'go', 'get', 'github.com/jessevdk/go-flags'
       system 'go', 'get', 'golang.org/x/text/transform'
       system 'go', 'get', 'golang.org/x/text/encoding/japanese'
