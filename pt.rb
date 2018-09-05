49776b270cd67962b6ac74d0d0fd67f70b3dae86
monochromegane <dev.kuro.obi@gmail.com>
2018-09-05 23:18:29 +0900

    Bumped version to 2.2.0.

--- pt.rb
+++ pt.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
-HOMEBREW_PT_VERSION='2.1.6'
+HOMEBREW_PT_VERSION='2.2.0'
 class Pt < Formula
   homepage 'https://github.com/monochromegane/the_platinum_searcher'
   url "https://github.com/monochromegane/the_platinum_searcher/releases/download/v#{HOMEBREW_PT_VERSION}/pt_darwin_amd64.zip"
-  sha256 'bd13aca0a762311c93f64c3c8256e04c6cf79faf8e6bb72c2d54deb4f874ac51'
+  sha256 '77f2c1b88b13b9e99246df6b119f35b58d617f28344a340d6897c36c9d23af75'
 
   version HOMEBREW_PT_VERSION
   head 'https://github.com/monochromegane/the_platinum_searcher.git', :branch => 'master'
